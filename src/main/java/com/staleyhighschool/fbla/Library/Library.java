1a110a545fecd225c195833173b243caad03c3a2
Connor Davis <poisonphang@gmail.com>
2018-02-17 22:02:56 -0600

    Updated comments and javadoc for Library.java

--- src/main/java/com/staleyhighschool/fbla/Library/Library.java
+++ src/main/java/com/staleyhighschool/fbla/Library/Library.java
@@ -4,14 +4,25 @@ import com.staleyhighschool.fbla.Database.Connector;
 import com.staleyhighschool.fbla.Users.User;
 import com.staleyhighschool.fbla.util.Enums;
 
+/**
+ * Drives most the larger actions taken in the application
+ */
 public class Library {
 
     public static Connector connection;
 
+    /**
+     * Creates the only instance of the connector object
+     */
     public Library() {
         connection = new Connector();
     }
 
+    /**
+     * Gets the current fine rate given the type of user
+     * @param user {@link User}
+     * @return {@link Double}
+     */
     public static double grabFineRate(User user) {
         double fineRate = 0;
         if (user.getAccountType() == Enums.AccountType.aSTUDENT) {
@@ -22,6 +33,11 @@ public class Library {
         return fineRate;
     }
 
+    /**
+     * Generates a count of overdue books for the given user
+     * @param user {@link User}
+     * @return {@link Integer}
+     */
     public static int lateBookCount(User user) {
         int lateBookCount = 0;
         for (Book book : user.getUserBooks()) {
