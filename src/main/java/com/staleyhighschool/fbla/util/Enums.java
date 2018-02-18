d48b43465ab3e7ddf163db5cc70bcd80a3f17a5c
Connor Davis <poisonphang@gmail.com>
2018-02-17 19:40:36 -0600

    Added Enums.java
    moved existing enums and created new ones in Enums.java

---
+++ src/main/java/com/staleyhighschool/fbla/util/Enums.java
@@ -0,0 +1,16 @@
+package com.staleyhighschool.fbla.util;
+
+public class Enums {
+
+    public enum IsOut {
+        IN, OUT
+    }
+
+    public enum IsLate {
+        LATE, SAFE
+    }
+
+    public enum AccountType {
+        aSTUDENT, aTEACHER
+    }
+}
